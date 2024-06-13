a12eba37027b8fc0aed1bdd5852ac329ec02766e
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-06-13 13:19:49 +0200

    add intro

---
+++ src/components/HomePage/Intro/card.jsx
@@ -0,0 +1,11 @@
+export function IntroCard({ icon, title, content }) {
+  return (
+    <div className="p-2 m-3 items-center w-full md:w-1/2  lg:w-1/4 p-4 relative flex">
+      <div>{icon}</div>
+      <div className="m-3">
+        <h2>{title}</h2>
+        <p className="text-black">{content}</p>
+      </div>
+    </div>
+  );
+}
--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,9 +1,11 @@
 import { Cover } from "./Cover";
+import { Intro } from "./Intro";
 
 export default function HomePageContent() {
   return (
     <div>
       <Cover />
+      <Intro />
     </div>
   );
 }

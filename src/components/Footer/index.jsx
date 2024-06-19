8853238bbc2dca45ffb29bf0aa0f34c6d022827b
Yen Nguyen <nguyenthikimyen25@gmail.com>
2024-06-19 12:29:46 +0200

    edit contact

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,16 +6,16 @@ export function Footer() {
   }
   return (
     <footer className="text-center bg-gray text-white">
-      <a href="/" >
+      <a href="/">
         {" "}
         <img src={logo} alt="holidaze logo" className="w-64 m-auto py-10" />
       </a>
       <nav>
         <ul className="flex justify-center">
           <li className="p-4 hover:underline">
-          <a href="#about">ABOUT US</a>
+            <a href="#about">ABOUT US</a>
           </li>
-         
+
           <li className="p-4 hover:underline">
             <a href="#contact">CONTACT</a>
           </li>
@@ -49,10 +49,26 @@ export function Footer() {
               d="M240.3 396.8c3.3 5.1 9.1 8.5 15.7 8.5s12.4-3.4 15.8-8.5L382 226.6c14.8-22.9 23.4-48.1 23.4-77.3C405.3 64.9 339 0 256 0S106.7 64.9 106.7 149.3c0 29.2 8.6 54.4 23.4 77.3zM256 64c47.1 0 85.3 38.2 85.3 85.3s-38.2 85.3-85.3 85.3s-85.3-38.2-85.3-85.3S208.9 64 256 64m109.4 259.5L256 469.3L146.6 323.5c-37.4 19.6-61.3 48.9-61.3 81.8C85.3 464.2 161.7 512 256 512s170.7-47.8 170.7-106.7c0-32.9-23.9-62.2-61.3-81.8"
             />
           </svg>
-          Walkers gate 4, 1771 Halden{" "}
+          <a
+            href="https://www.google.com/maps/place/Tista+Sushi/@59.1210181,11.3793512,16z/data=!3m1!4b1!4m6!3m5!1s0x464412dd19ebdb45:0x9f2c80054580135d!8m2!3d59.1210181!4d11.3793512!16s%2Fg%2F11gbkxj6j9?entry=ttu"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-2 hover:underline"
+          >
+            Walkers gate 4, 1771 Halden
+          </a>{" "}
         </div>
       </div>
-      <p className="my-5">&copy; 2024 Powered by <a class="email" href="mailto:contact@orderintelligence.no" className="text-primary hover:underline">Order Intelligence</a></p>
+      <p className="my-5">
+        &copy; 2024 Powered by{" "}
+        <a
+          class="email"
+          href="mailto:contact@orderintelligence.no"
+          className="text-primary hover:underline"
+        >
+          Order Intelligence
+        </a>
+      </p>
     </footer>
   );
 }
